f50239c8b8174e2f70ea7e4072d821b1bb65cc81
lucastu <86346920+lucastu@users.noreply.github.com>
2022-06-23 12:45:30 +0200

    Update small.ino

--- small/small.ino
+++ small/small.ino
@@ -1,3 +1,5 @@
+// https://www.hackster.io/mjrobot/controlling-a-roomba-robot-with-arduino-and-android-device-56970d
+
 #include "Arduino.h"
 #include "roombaDefines.h"
 
@@ -6,7 +8,7 @@
 
 void setup()
 {
-  Roomba.begin(19200);
+  Roomba.begin(115200);
   wakeUp();   // Wake-up Roomba
   startSafe(); // Start Roomba in Safe Mode
   playSound(2); 
